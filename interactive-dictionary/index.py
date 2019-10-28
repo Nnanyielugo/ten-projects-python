bbdaa9927cf7455eb2dd23498d6560abec762ff5
Nnanyielugo <kz.onyegbule@gmail.com>
2019-10-28 09:02:49 +0100

    format output strings

--- interactive-dictionary/index.py
+++ interactive-dictionary/index.py
@@ -6,16 +6,18 @@ def dictionary():
 
   word = input("Enter a word to search for: ")
   
+  # handle no inputs
   while not word or word == '':
     print("You did not enter any value!")
     word = input("Enter a word to search for: ")
 
+  # handle words not in dict
   if not word in json_data:
     print("The word you searched for does not exist!")
     return
 
   definitions = json_data[word]
   for index, definition in enumerate(definitions):
-    print(index + 1, definition)
+    print('{}. {}'.format(index + 1, definition))
 
 dictionary()
