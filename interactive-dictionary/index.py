25b512cfa475996d0c515d8b5db2cd39006cd1f8
Nnanyielugo <kz.onyegbule@gmail.com>
2019-10-28 10:21:56 +0100

    handle user response
    pretty-print output

--- interactive-dictionary/index.py
+++ interactive-dictionary/index.py
@@ -1,4 +1,5 @@
 import json
+from difflib import get_close_matches
 
 def dictionary():
   source = open("data.json")
@@ -8,17 +9,40 @@ def dictionary():
   word = word.lower()
   
   # handle no inputs
-  while not word or word == '':
-    print("You did not enter any value!")
-    word = input("Enter a word to search for: ")
-
-  # handle words not in dict
-  if not word in json_data:
-    print("The word you searched for does not exist!")
-    return
+  while not word or word not in json_data:
+    # handle words not in dict
+    if word:
+      if not word in json_data:
+        close_matches = get_close_matches(word, json_data.keys(), cutoff=0.8)
+        if close_matches:
+          # make suggestion and substitute word for suggestion if user wishes
+          suggestion = close_matches[0]
+          print("Word not found.\nDid you mean {}?".format(suggestion))
+          prompt = input("Yes/No, or 'exit' to quit: ")
+          if prompt == "Yes":
+            word = suggestion
+          elif prompt == "NO":
+            print("The word you searched for does not exist!")
+            return
+          elif prompt == "exit":
+            print("Quitting now...")
+            return
+          else:
+            print("We did not understand your response. Please try again")
+            word = input("Enter a word to search for: ")
+        else:
+          print("The word you searched for does not exist!")
+          return
+    else:
+      print("You did not enter any value!")
+      word = input("Enter a word to search for: ")
 
   definitions = json_data[word]
+  # prettify output
+  print()
+  print("{}: ".format(word.capitalize()))
   for index, definition in enumerate(definitions):
     print('{}. {}'.format(index + 1, definition))
+  print()
 
 dictionary()
